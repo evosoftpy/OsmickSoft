869689bc0060d0b699439955d6b89f54a3c5774a
gp753 <gp_753@hotmail.com>
2016-10-31 01:22:07 -0300

    ok

--- nuevo repuestos/editarProveedores.vb
+++ nuevo repuestos/editarProveedores.vb
@@ -13,8 +13,8 @@
         Dim t As New herramientas
         i = moduloDatos.i
         clienteNomTxt.Text = DataSet1.Tables("proveedor").Rows(i).Item("nombre_proveedor")
-        clienteApTxt.Text = DataSet1.Tables("proveedor").Rows(i).Item("ruc_proveedor")
-        clienteRucTxt.Text = DataSet1.Tables("proveedor").Rows(i).Item("numero_proveedor")
+        clienteApTxt.Text = DataSet1.Tables("proveedor").Rows(i).Item("numero_proveedor")
+        clienteRucTxt.Text = DataSet1.Tables("proveedor").Rows(i).Item("ruc_proveedor")
 
     End Sub
 
@@ -25,30 +25,49 @@
     End Sub
 
     Private Sub clienteEditarBtn_Click(sender As Object, e As EventArgs) Handles clienteEditarBtn.Click
+
+
         Dim i As Integer
         'Dim f1 As New Cliente
         i = moduloDatos.i
+
+        Dim ban_existe As Integer
+        Dim a As Integer
+        ban_existe = 0
+
         Try
 
+            For a = 0 To DataSet1.Tables("proveedor").Rows.Count - 1
+                If a <> i And DataSet1.Tables("proveedor").Rows(a).Item("ruc_proveedor") = clienteRucTxt.Text Then
+                    ban_existe = 1
+                End If
+            Next
+            If ban_existe = 0 Then
+                DataSet1.Tables("proveedor").Rows(i).Item("nombre_proveedor") = clienteNomTxt.Text
+                DataSet1.Tables("proveedor").Rows(i).Item("ruc_proveedor") = clienteRucTxt.Text
+                DataSet1.Tables("proveedor").Rows(i).Item("numero_proveedor") = clienteApTxt.Text
 
-            DataSet1.Tables("proveedor").Rows(i).Item("nombre_proveedor") = clienteNomTxt.Text
-            DataSet1.Tables("proveedor").Rows(i).Item("ruc_proveedor") = clienteApTxt.Text
-            DataSet1.Tables("proveedor").Rows(i).Item("numero_proveedor") = clienteRucTxt.Text
+                Validate()
+                ProveedorBindingSource.EndEdit()
+                ProveedorTableAdapter.Update(DataSet1.proveedor)
+                MsgBox("Proveedor editado con éxito")
 
-            Validate()
-            ProveedorBindingSource.EndEdit()
-            ProveedorTableAdapter.Update(DataSet1.proveedor)
-            MsgBox("Proveedor editado con éxito")
+                clienteNomTxt.Text = ""
+                clienteApTxt.Text = ""
+                clienteRucTxt.Text = ""
 
-            clienteNomTxt.Text = ""
-            clienteApTxt.Text = ""
-            clienteRucTxt.Text = ""
+                '   f1.Show()
+                form_manager.prov.Button1.PerformClick()
 
-            '   f1.Show()
-            form_manager.prov.Button1.PerformClick()
+                Me.Hide()
+                moduloDatos.reset = True
+            Else
+                MsgBox("Ruc del proveedor ya existe")
+                clienteRucTxt.Focus()
 
-            Me.Hide()
-            moduloDatos.reset = True
+
+            End If
+
         Catch ex As Exception
             MsgBox("Error durante la edición")
 
